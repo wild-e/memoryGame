402713513f992f7f51e953a3d92557eddcd8cfe3
wild-e <wild.emmanuelle@gmail.com>
2020-10-11 20:50:11 +0200

    set up basic CSS and HTML

---
+++ index.php
@@ -0,0 +1,55 @@
+<!-- memory game
+
+On load Afficher 16 div carrées bords arrondis dans un parent en grid tout centré (responsive) de façon random
+Dans chaque div une image svg style ravensburger 
+Quand on click sur la div il faut que l'image change et affiche l'image qui a été randomisée
+quand on click sur une autre div elle doit se retourner
+si les noms des deux image sont les mêmes prompt "bien joué"
+si les noms des deux images sont différentes elle reprennent leur premier état -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Memory game | Emmanuelle WILD | Dev Web</title>
+  <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+
+    <div class="memoryParent">
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+        <div class="card">
+        </div>
+    </div>
+
+  </body>
+</html>
